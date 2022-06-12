cca23ef1f7dd3a2c91517cf10b50ca0ad60da1b5
Moayad <moayad.hakim@gmail.com>
2022-06-12 12:20:26 +0300

    update the profile

--- flutter_application_training/lib/profile.dart
+++ flutter_application_training/lib/profile.dart
@@ -1,173 +1,285 @@
-import 'package:flutter/cupertino.dart';
+import '../flutter_flow/flutter_flow_icon_button.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
-import 'NavBar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Myprofile extends StatelessWidget {
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({Key key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home:Directionality(textDirection: TextDirection.rtl, child: Scaffold(
-          appBar: AppBar(
-            title: Text("profile"),
-          ),
-          drawer: NavBar(),
-          body: MyProfilePage(),
-        ))) ;
-  }
+  _HomePageWidgetState createState() => _HomePageWidgetState();
 }
 
-class MyProfilePage extends StatefulWidget {
-  MyProfilePage({Key? key}) : super(key: key);
+class _HomePageWidgetState extends State<HomePageWidget> {
+  TextEditingController textController1;
+  TextEditingController textController2;
+  TextEditingController textController3;
+  TextEditingController textController4;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
-  _MyProfilePageState createState() => _MyProfilePageState();
-}
-
-class _MyProfilePageState extends State<MyProfilePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: profileView());
+  void initState() {
+    super.initState();
+    textController1 = TextEditingController();
+    textController2 = TextEditingController();
+    textController3 = TextEditingController();
+    textController4 = TextEditingController();
   }
 
-  Widget profileView() {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.fromLTRB(30, 50, 30, 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-          ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: FlutterFlowTheme.of(context).primaryColor,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Profile Page',
+          style: FlutterFlowTheme.of(context).title1,
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
-          child: Stack(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 70,
-                child: ClipOval(
-                  child: Image.asset(
-                    'images/img.jpg',
-                    height: 150,
-                    width: 150,
-                    fit: BoxFit.cover,
+        actions: [],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [],
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 0),
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.network(
+                    'https://picsum.photos/seed/144/600',
                   ),
                 ),
               ),
-              Positioned(
-                  bottom: 1,
-                  right: 1,
-                  child: Container(
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 1),
+                child: FlutterFlowIconButton(
+                  borderColor: Colors.transparent,
+                  borderRadius: 30,
+                  borderWidth: 1,
+                  buttonSize: 60,
+                  icon: Icon(
+                    Icons.linked_camera,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    print('IconButton pressed ...');
+                  },
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-0.4, 0),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(15, 10, 15, 10),
+                  child: TextFormField(
+                    controller: textController1,
+                    onChanged: (_) => EasyDebounce.debounce(
+                      'textController1',
+                      Duration(milliseconds: 2000),
+                          () => setState(() {}),
+                    ),
+                    autofocus: true,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      labelText: 'Full Name',
+                      hintText: 'Enter your name',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                          width: 1,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                          width: 1,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.person,
+                      ),
+                    ),
+                    style: FlutterFlowTheme.of(context).bodyText1,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(15, 0, 15, 10),
+                child: TextFormField(
+                  controller: textController2,
+                  onChanged: (_) => EasyDebounce.debounce(
+                    'textController2',
+                    Duration(milliseconds: 2000),
+                        () => setState(() {}),
+                  ),
+                  autofocus: true,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your Email',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.mail_outlined,
+                    ),
+                  ),
+                  style: FlutterFlowTheme.of(context).bodyText1,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(15, 0, 15, 10),
+                child: TextFormField(
+                  controller: textController3,
+                  onChanged: (_) => EasyDebounce.debounce(
+                    'textController3',
+                    Duration(milliseconds: 2000),
+                        () => setState(() {}),
+                  ),
+                  autofocus: true,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    hintText: 'Enter your phone number',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.phone_outlined,
+                    ),
+                  ),
+                  style: FlutterFlowTheme.of(context).bodyText1,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(15, 0, 15, 60),
+                child: TextFormField(
+                  controller: textController4,
+                  onChanged: (_) => EasyDebounce.debounce(
+                    'textController4',
+                    Duration(milliseconds: 2000),
+                        () => setState(() {}),
+                  ),
+                  autofocus: true,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    labelText: 'Grade',
+                    hintText: 'Enter your grade',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1,
+                      ),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.school_sharp,
+                    ),
+                  ),
+                  style: FlutterFlowTheme.of(context).bodyText1,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(30, 0, 30, 0),
+                child: FFButtonWidget(
+                  onPressed: () {
+                    print('Button pressed ...');
+                  },
+                  text: 'Update Profile',
+                  options: FFButtonOptions(
+                    width: 150,
                     height: 40,
-                    width: 40,
-                    child: Icon(
-                      Icons.add_a_photo,
+                    color: FlutterFlowTheme.of(context).primaryColor,
+                    textStyle: FlutterFlowTheme.of(context).subtitle2.override(
+                      fontFamily: 'Poppins',
                       color: Colors.white,
                     ),
-                    decoration: BoxDecoration(
-                        color: Colors.deepOrange,
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                  ))
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1,
+                    ),
+                    borderRadius: 12,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
-        Expanded(
-            child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [Colors.black54, Color.fromRGBO(0, 41, 102, 1)])),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
-                child: Container(
-                  height: 60,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        ' Student Name',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      border: Border.all(width: 1.0, color: Colors.white70)),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-                child: Container(
-                  height: 60,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Email',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      border: Border.all(width: 1.0, color: Colors.white70)),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-                child: Container(
-                  height: 60,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Your grade',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      border: Border.all(width: 1.0, color: Colors.white70)),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-                child: Container(
-                  height: 60,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Phone number',
-                        style: TextStyle(color: Colors.white70),
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      border: Border.all(width: 1.0, color: Colors.white70)),
-                ),
-              ),
-            ],
-          ),
-        ))
-      ],
+      ),
     );
   }
 }
